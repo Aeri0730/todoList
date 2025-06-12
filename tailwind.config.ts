b87b417637e8248658a8d5e523a5e0c5f9e7586b
aeri0730 <dodl8549@gmail.com>
2025-06-12 15:53:10 +0900

    setting : tailwind.config.ts 추가 (#4)

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    // Shadcn UI 컴포넌트 경로 (init 시 설정에 따라 다를 수 있음)
+    "./@/components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
