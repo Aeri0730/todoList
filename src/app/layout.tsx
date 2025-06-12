e07ecca8afabd70992dd671ccd97f79edb57f016
aeri0730 <dodl8549@gmail.com>
2025-06-12 13:02:44 +0900

    setting : shadcn 설치 및 tanstack query 코드 설정 (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Providers from "@/components/Providers";
 
 const queryClient = new QueryClient();
 
@@ -15,12 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <html lang="ko">
-        <body className="font-sans antialiased text-gray-800 bg-gray-50 min-h-screen flex items-center justify-center p-4 sm:p-6 md:p-8">
-          {children}
-        </body>
-      </html>
-    </QueryClientProvider>
+    <html lang="ko">
+      <body className="font-sans antialiased text-gray-800 bg-gray-50 min-h-screen flex items-center justify-center p-4 sm:p-6 md:p-8">
+        <Providers>{children} </Providers>
+      </body>
+    </html>
   );
 }
